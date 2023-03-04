8001942636d8dc978f1070ff2cd9404d9ccfd09a
Winwin <em.ireri41@gmail.com>
2023-03-04 17:30:45 +0300

    Update VencordTab.tsx

--- src/components/VencordSettings/VencordTab.tsx
+++ src/components/VencordSettings/VencordTab.tsx
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import { readFileSync } from 'fs';
 import { useSettings } from "@api/settings";
 import { classNameFactory } from "@api/Styles";
 import DonateButton from "@components/DonateButton";
